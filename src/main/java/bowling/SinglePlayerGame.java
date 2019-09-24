206bcb88829a6d0ba2bdfa4cfdf991b4006a8877
clement.attie@etud.univ-jfc.fr <>
2019-09-25 00:50:11 +0200

    rendu TP3

--- src/main/java/bowling/SinglePlayerGame.java
+++ src/main/java/bowling/SinglePlayerGame.java
@@ -1,16 +1,19 @@
 package bowling;
 
+import java.util.ArrayList;
+
 /**
  * Cette classe a pour but d'enregistrer le nombre de quilles abattues lors des
  * lancés successifs d'<b>un seul et même</b> joueur, et de calculer le score
  * final de ce joueur
  */
 public class SinglePlayerGame {
-
+        private final ArrayList<Integer> quilleWin = new ArrayList<>();
+        
 	/**
 	 * Constructeur
 	 */
-	public SinglePlayerGame() {
+	public SinglePlayerGame() {            
 	}
 
 	/**
@@ -18,9 +21,10 @@ public class SinglePlayerGame {
 	 *
 	 * @param nombreDeQuillesAbattues le nombre de quilles abattues lors de
 	 * ce lancé
+         * On ajoute le nombre de quille tombé à chaques lancé dans une arraylist
 	 */
 	public void lancer(int nombreDeQuillesAbattues) {
-
+                quilleWin.add(nombreDeQuillesAbattues);
 	}
 
 	/**
@@ -29,6 +33,25 @@ public class SinglePlayerGame {
 	 * @return Le score du joueur
 	 */
 	public int score() {
-		throw new UnsupportedOperationException("Pas encore implémenté");
+		int score = 0;
+		int tourActuel = 0;
+		
+                for (int i = 0; i < 10; i++){
+                    
+                    if(quilleWin.get(tourActuel) == 10){
+                        score += 10 + (quilleWin.get(tourActuel+1) + quilleWin.get(tourActuel+2));
+                        tourActuel ++;
+                    }
+                    
+                    else if ((quilleWin.get(tourActuel) + quilleWin.get(tourActuel+1)) == 10){
+				score += 10 + quilleWin.get(tourActuel+2);
+				tourActuel += 2;
+                    }
+                    else{
+                        score += (quilleWin.get(tourActuel) + quilleWin.get(tourActuel+1));
+			tourActuel += 2;
+                    }
+		}
+		return score;
 	}
 }
